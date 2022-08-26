944a04657c6ddaa3392b67b1a5070920e4deb1da
Karan Gala <galakaran47@gmail.com>
2022-08-26 15:13:13 -0400

    Adding main.py

---
+++ code/main.py
@@ -0,0 +1,14 @@
+class Operations
+	def calculator(swi, n1, n2):
+		if swi == 1:
+			return (n1+n2)
+		elif swi == 2:
+			return (n1-n2)
+		elif swi == 3:
+			return (n1*n2)
+		elif swi == 4:
+			if n2 == 0:
+				return ("Error denominator can not be zero")
+			else:
+				return (n1/n2)
+		return "EXITING..."
