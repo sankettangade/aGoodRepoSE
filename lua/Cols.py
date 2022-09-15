b438fa7f6461ac90642971dd284a0840d82f2ca7
= <=>
2022-09-14 20:24:53 -0400

    Add Column Class

---
+++ lua/Cols.py
@@ -0,0 +1,36 @@
+import re
+
+class Cols:
+
+	def __init__ (self, names):
+
+		self.name = names
+		self.all = {}
+		self.klass = None
+		self.x = {}
+		self.y = {}
+		for ele in names:
+			c,s = ele[0],ele[1]
+			# print(c,s)
+			self.all[s] = c
+
+			# Sorts the Num type here
+			if(s[0].isupper()):
+				self.x[s] = c
+			# Sorts for the columns to be skipped
+			elif(re.search(":$",s)):
+				continue
+			#Sorts the Sym type here
+			elif(s[0].islower()):
+				self.y[s] = c
+			# elif(re.search("!",s[-1])):
+			# 	self.klass[s] = c
+			# If needed to classify independent and dependent for testing.
+			# elif(re.search("[!+−]",s)):
+			# 	self.x[s] = c
+
+
+
+
+
+
