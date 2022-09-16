0358d60ded7a44fbbc3393944e5c89b8e3a373fb
Karan Gala <[galakaran47@gmail.com]>
2022-09-16 01:37:23 -0400

    Resolve Errors in Data.py

--- lua/Data.py
+++ lua/Data.py
@@ -1,75 +1,69 @@
-from re import split
 import Cols
 import Utils
-import Num
-import Sym
-import The
+
 
 class Data:
-    def __init__(self,src) -> None:
+    def __init__(self, src) -> None:
         self.cols = None
         self.rows = []
+        self.mapping = {}
         if type(src) == str:
-            Utils.csv(src,self)
-            self.add_to_columns()
+            Utils.csv(src, self)
 
-    def add(self,xs):
+    def add(self, xs):
         if not self.cols:
             self.cols = Cols.Cols(xs)
-            temp = self.cols.x | self.cols.y 
+            temp = {**self.cols.x,**self.cols.y}
             l = []
+            indices = list(temp.values())
+            for i in range(0,len(indices)):
+                indices[i] = indices[i][0]
+            indices.sort()
             self.rows.append(l)
-            for i in range(len(xs)):
-                if i in temp.values():
-                    l.append(xs[i])
+            count = 0
+            for i in indices:
+                l.append(xs[i])
+                self.mapping[i] = count
+                count+=1
+
         else:
             l = []
-            temp = self.cols.x | self.cols.y 
+            indices_x, indices_y = list(self.cols.x.values()), list(self.cols.y.values())
+        
+            for i in range(0,len(indices_x)):
+                col_obj = indices_x[i][1]
+                indices_x[i] = indices_x[i][0]
+            for i in range(0,len(indices_y)):
+                indices_y[i] = indices_y[i][0]
+
             for i in range(len(xs)):
-                if i in self.cols.x.values():
+                if i in indices_x:
                     if xs[i] != "?":
                         l.append(float(xs[i]))
+                        self.cols.x[self.rows[0][self.mapping[i]]][1].add(float(xs[i]))
                     else:
                         l.append(xs[i])
-                elif i in self.cols.y.values():
-                    l.append(xs[i])
+                        self.cols.x[self.rows[0][self.mapping[i]]][1].add(xs[i])
+                elif i in indices_y:
+                    l.append(float(xs[i]))                    
+                    self.cols.y[self.rows[0][self.mapping[i]]][1].add(float(xs[i]))
             self.rows.append(l)
-            
-            
-        
-    # def add_to_columns(self):
-    #     for col_name, col_index in self.cols.y.items():
-    #         s = Sym.Sym(col_name, col_index)
-    #         col_index = self.rows[0].index(col_name)
-    #         for i in range(1,len(self.rows)-1):
-    #             s.add(self.rows[i][col_index])
-    #         The.column_objects[col_name] = s
-    #
-    #     for col_name, col_index in self.cols.x.items():
-    #         s = Num.Num(col_name, col_index)
-    #         col_index = self.rows[0].index(col_name)
-    #         for i in range(1,len(self.rows)-1):
-    #             # print((self.rows[i]))
-    #             s.add(self.rows[i][col_index])
-    #         The.column_objects[col_name] = s
 
     def print(self):
         print(self.rows)
 
-    def stats(self, showCols,fun = "mid" ):
+    def stats(self, showCols, fun="mid"):
         temp = {}
         showCols = self.cols.x if showCols == None else self.cols.y
         for col in showCols:
-            obj = The.column_objects[col]
             if fun == "mid":
-                temp[col] = obj.mid()
+                temp[col] = showCols[col][1].mid()
             elif fun == "div":
-                temp[col] = obj.div()
+                temp[col] = showCols[col][1].div()
 
         return temp
 
 
-
 a = Data("../data/lua_sample_data.csv")
-
+# print(a.rows)
 print(a.stats(None, "div"))
