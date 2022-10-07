5a2efe1e95eaa2b6f4bb24e07088f9b739774762
Karan Gala <[galakaran47@gmail.com]>
2022-10-06 20:02:21 -0400

    Update testCsv.py

--- test/testCsv.py
+++ test/testCsv.py
@@ -8,17 +8,17 @@ class TestReadCsv:
 
     def testCsv(self):
         d = Data.Data()
-        Utils.csv("/Users/sankettangade/Documents/SE/aGoodRepoSE/data/lua_sample_data.csv", d)
+        Utils.csv("../data/lua_sample_data.csv", d)
         for i in range(0, 11):
                 print("Row No.",i," = ", d.rows[i])
 
     def testData(self):
-        data = Data.Data("/Users/sankettangade/Documents/SE/aGoodRepoSE/data/lua_sample_data.csv")
+        data = Data.Data("../data/lua_sample_data.csv")
         for i, x in enumerate(data.rows):
             print(i, "Value = ", x)
 
     def testColumn(self):
-        data1 = Data.Data("/Users/sankettangade/Documents/SE/aGoodRepoSE/data/lua_sample_data.csv")
+        data1 = Data.Data("../data/lua_sample_data.csv")
 
         print("xmid = ", data1.stats(data1.cols.x, "mid"))
         print("ymid = ", data1.stats(data1.cols.y, "mid"))
